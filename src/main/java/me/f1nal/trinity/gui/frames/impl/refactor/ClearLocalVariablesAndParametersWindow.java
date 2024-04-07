68cbe8b2f51a67e94e277594eaa3d76eb23d8301
Wykt <wonkt@proton.me>
2024-04-07 05:08:05 +0200

    add: warning

--- src/main/java/me/f1nal/trinity/gui/frames/impl/refactor/ClearLocalVariablesAndParametersWindow.java
+++ src/main/java/me/f1nal/trinity/gui/frames/impl/refactor/ClearLocalVariablesAndParametersWindow.java
@@ -32,6 +32,8 @@ public final class ClearLocalVariablesAndParametersWindow extends StaticWindow {
 
     private void runRefactor(final Execution execution) {
         remover.runRefactor(execution);
+
+        // FIXME: does not refresh decompiler text...
         Main.getEventBus().post(new EventRefreshDecompilerText(dc -> true));
     }
 }
